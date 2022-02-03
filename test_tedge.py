52210e576481e4bf1b4b8942fb6a5e68297dd3bf
Michael Abel <info@abel-ikt.de>
2022-02-03 20:17:26 +0100

    Format

--- test_tedge.py
+++ test_tedge.py
@@ -1,5 +1,4 @@
-
-# pip install ../pytest_system_test_plugin 
+# pip install ../pytest_system_test_plugin
 
 import logging
 import os
@@ -14,33 +13,33 @@ import pytest
 def test_go():
     pass
 
+
 def test_use_case_help(process_factory):
 
     help = process_factory(["tedge", "-V"])
     help.run()
     assert help.get_stdout() == "tedge 0.5.2"
 
+
 def test_use_connect(process_factory):
 
     con = process_factory(["sudo", "tedge", "connect", "c8y"])
     con.run()
 
-    print( con.get_stdout() )
-    print( con.get_stderr( ))
+    print(con.get_stdout())
+    print(con.get_stderr())
     assert con.returncode == 0
 
     discon = process_factory(["sudo", "tedge", "disconnect", "c8y"])
     discon.run()
 
-    print( discon.get_stderr() )
-    print( discon.get_stdout() )
+    print(discon.get_stderr())
+    print(discon.get_stdout())
 
     assert discon.returncode == 0
 
 
-
 def test_use_case_connect_and_observe(process_factory):
-
 
     mos = process_factory(["/usr/bin/mosquitto_sub", "-v", "-t", "#"], name="mos")
     mos.run_bg()
@@ -48,15 +47,15 @@ def test_use_case_connect_and_observe(process_factory):
     con = process_factory(["sudo", "tedge", "connect", "c8y"], name="con")
     con.run()
 
-    print( con.get_stdout() )
-    print( con.get_stderr( ))
+    print(con.get_stdout())
+    print(con.get_stderr())
     assert con.returncode == 0
 
     discon = process_factory(["sudo", "tedge", "disconnect", "c8y"], name="discon")
     discon.run()
 
-    print( discon.get_stderr() )
-    print( discon.get_stdout() )
+    print(discon.get_stderr())
+    print(discon.get_stdout())
 
     assert discon.returncode == 0
 
@@ -66,3 +65,5 @@ def test_use_case_connect_and_observe(process_factory):
     assert "tedge/commands/req/software/list" in mos.get_stdout()
 
 
+# TODO tedge connect in the background ?
+# TODO inverser gherkin here?
